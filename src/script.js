be1f2a749356278de4012c6f7f710d7d5ae72c64
zhuzhenquan <2332778528@qq.com>
2023-05-24 16:58:21 +0800

    fix: specify the encoding format

--- src/script.js
+++ src/script.js
@@ -10,7 +10,8 @@ const NAME = process.env.BSAE_NAME;
 // https://blog.csdn.net/weixin_42081389/article/details/100543007
 
 // const script = `ffmpeg -f gdigrab -i desktop -vcodec libx264 -preset:v ultrafast -tune:v zerolatency -f rtsp rtsp://${IP}:${PORT}/${NAME}`;
-// ffmpeg -f gdigrab -r 30 -i desktop screen.avi  录制屏幕
+// ffmpeg -f gdigrab -r 30 -i desktop screen.avi  录制屏幕 avi格式
+// ffmpeg -f gdigrab -r 30 -i desktop -vcodec mpeg4 out.mp4  录制固定格式的mp4 win10播放器可播放
 // ffmpeg -i screen.avi -c copy -map 0 video.mp4  avi 转 mp4
 
 const script = `ffmpeg -f gdigrab -i desktop -pix_fmt yuv420p -c:v libx264 -preset ultrafast -b:v 600k -c:a aac -b:a 160k -f rtsp rtsp://${IP}:${PORT}/${NAME}`;
