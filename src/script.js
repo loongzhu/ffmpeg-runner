82a16d519de7c9ed1571aa5d08344218d9a78f42
zhuzhenquan <2332778528@qq.com>
2023-05-24 16:26:16 +0800

    docs: add common scripts

--- src/script.js
+++ src/script.js
@@ -7,7 +7,11 @@ const IP = process.env.BASE_IP;
 const PORT = process.env.BASE_PROT;
 const NAME = process.env.BSAE_NAME;
 
+// https://blog.csdn.net/weixin_42081389/article/details/100543007
+
 // const script = `ffmpeg -f gdigrab -i desktop -vcodec libx264 -preset:v ultrafast -tune:v zerolatency -f rtsp rtsp://${IP}:${PORT}/${NAME}`;
+// ffmpeg -f gdigrab -r 30 -i desktop -map 0 screen.mp4  录制屏幕
+// ffmpeg -i screen.avi -c copy -map 0 video.mp4  avi 转 mp4
 
 const script = `ffmpeg -f gdigrab -i desktop -pix_fmt yuv420p -c:v libx264 -preset ultrafast -b:v 600k -c:a aac -b:a 160k -f rtsp rtsp://${IP}:${PORT}/${NAME}`;
 
