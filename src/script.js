2a9da3bdb53125bfc249b48af872492aaa73edf1
zhuzhenquan <2332778528@qq.com>
2023-05-24 16:28:56 +0800

    fix: scripts

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ const NAME = process.env.BSAE_NAME;
 // https://blog.csdn.net/weixin_42081389/article/details/100543007
 
 // const script = `ffmpeg -f gdigrab -i desktop -vcodec libx264 -preset:v ultrafast -tune:v zerolatency -f rtsp rtsp://${IP}:${PORT}/${NAME}`;
-// ffmpeg -f gdigrab -r 30 -i desktop -map 0 screen.mp4  录制屏幕
+// ffmpeg -f gdigrab -r 30 -i desktop screen.avi  录制屏幕
 // ffmpeg -i screen.avi -c copy -map 0 video.mp4  avi 转 mp4
 
 const script = `ffmpeg -f gdigrab -i desktop -pix_fmt yuv420p -c:v libx264 -preset ultrafast -b:v 600k -c:a aac -b:a 160k -f rtsp rtsp://${IP}:${PORT}/${NAME}`;
