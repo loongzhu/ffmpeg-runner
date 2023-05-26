098667b6cb77e1c549a0ea52d0d52b43ec7c6fcd
zhuzhenquan <2332778528@qq.com>
2023-05-26 11:37:12 +0800

    upgrade: func copy

--- src/copy.js
+++ src/copy.js
@@ -3,38 +3,40 @@ const path = require("path");
 const fse = require("fs-extra");
 const process = require("process");
 
-// 获取源文件
-const sourceFile = path.join(process.cwd(), ".env");
+const sourceENV = path.join(process.cwd(), ".env");
+const targetENV = path.join(process.cwd(), "./dist/.env");
+copyFile(sourceENV, targetENV, "./dist/.env");
 
-// 获取目标文件
-const targetFile = path.join(process.cwd(), "./dist/.env");
+const sourceHtml = path.join(process.cwd(), "./src/live.html");
+const targetHtml = path.join(process.cwd(), "./dist/live.html");
+copyFile(sourceHtml, targetHtml, "./dist/live.html");
 
-copyFile();
+const sourceFolder = path.join(process.cwd(), "./src/assets");
+const targetFolder = path.join(process.cwd(), "./dist/assets");
+copyFolder(sourceFolder, targetFolder, "./dist/assets");
 
 /**
  * 复制文件
+ * @param {*} source 源文件
+ * @param {*} target 目标文件
+ * @param {*} name 目标文件名
  */
-function copyFile() {
-  fs.copyFile(sourceFile, targetFile, (err) => {
+function copyFile(source, target, name) {
+  fs.copyFile(source, target, (err) => {
     if (err) return console.error(err);
-    console.log("./dist/.env This file is copied successfully!");
+    console.log(`${name} This file is copied successfully!`);
   });
 }
 
-// 获取源文件夹
-const sourceFolder = path.join(process.cwd(), "./src/assets");
-
-// 获取目标文件夹
-const targetFolder = path.join(process.cwd(), "./dist/assets");
-
-copyFolder();
-
 /**
  * 复制文件夹
+ * @param {*} source 源文件夹
+ * @param {*} target 目标文件夹
+ * @param {*} name 目标文件夹名
  */
-function copyFolder() {
-  fse.copy(sourceFolder, targetFolder, (err) => {
+function copyFolder(source, target, name) {
+  fse.copy(source, target, (err) => {
     if (err) return console.error(err);
-    console.log("./dist/assets This folder is copied successfully!");
+    console.log(`${name} This folder is copied successfully!`);
   });
 }
